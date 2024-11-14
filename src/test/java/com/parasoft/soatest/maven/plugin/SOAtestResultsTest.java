bac12241d88a82fd9b2c4eb4f66e36c2425a1300
Roberto Nares <rnares@parasoft.com>
2024-11-14 14:57:20 -0800

    Add unit tests for SOAtestResults class

---
+++ src/test/java/com/parasoft/soatest/maven/plugin/SOAtestResultsTest.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2024 Parasoft Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.parasoft.soatest.maven.plugin;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import javax.xml.stream.XMLStreamException;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+
+public class SOAtestResultsTest {
+    @Test
+    public void parseReportXML_WhenReportHasZeroTests_ReturnsTotalZeroAndFailedZero() throws Exception {
+        File report = new File("target/test-classes/report_with_total_zero.xml");
+        assertNotNull(report);
+        assertTrue(report.exists());
+
+        SOAtestResults results = SOAtestResults.parseReportXML(report);
+        assertEquals(results.getTotal(), 0);
+        assertEquals(results.getFailed(), 0);
+    }
+
+    @Test
+    public void parseReportXML_WhenReportHasTenTestsAndAllPass_ReturnsTotalTenAndFailedZero() throws Exception {
+        File report = new File("target/test-classes/report_with_total_ten_pass_ten.xml");
+        assertNotNull(report);
+        assertTrue(report.exists());
+
+        SOAtestResults results = SOAtestResults.parseReportXML(report);
+        assertEquals(results.getTotal(), 10);
+        assertEquals(results.getFailed(), 0);
+    }
+
+    @Test
+    public void parseReportXML_WhenReportHasTenTestsAndTwoFail_ReturnsTotalTenAndFailedTwo() throws Exception {
+        File report = new File("target/test-classes/report_with_total_ten_fail_two.xml");
+        assertNotNull(report);
+        assertTrue(report.exists());
+
+        SOAtestResults results = SOAtestResults.parseReportXML(report);
+        assertEquals(results.getTotal(), 10);
+        assertEquals(results.getFailed(), 2);
+    }
+
+    @Test
+    public void writeFailsafeSummary_WhenReportHasZeroTests_WritesCompletedZero() throws Exception {
+        File report = new File("target/test-classes/report_with_total_zero.xml");
+        assertNotNull(report);
+        assertTrue(report.exists());
+
+        File expectedSummary = new File("target/test-classes/failsafe_summary_completed_zero.xml");
+        assertNotNull(expectedSummary);
+        assertTrue(expectedSummary.exists());
+
+        Path actualSummary = Paths.get("target/test-classes/failsafe-summary.xml");
+        assertNotNull(actualSummary);
+
+        SOAtestResults results = SOAtestResults.parseReportXML(report);
+        results.writeFailsafeSummary(actualSummary);
+
+        String actual = new String(Files.readAllBytes(actualSummary));
+        String expected = new String(Files.readAllBytes(expectedSummary.toPath()));
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void writeFailsafeSummary_WhenReportHasTenTestsAndAllPass_WritesCompletedTen() throws Exception {
+        File report = new File("target/test-classes/report_with_total_ten_pass_ten.xml");
+        assertNotNull(report);
+        assertTrue(report.exists());
+
+        File expectedSummary = new File("target/test-classes/failsafe_summary_completed_ten.xml");
+        assertNotNull(expectedSummary);
+        assertTrue(expectedSummary.exists());
+
+        Path actualSummary = Paths.get("target/test-classes/failsafe-summary.xml");
+        assertNotNull(actualSummary);
+
+        SOAtestResults results = SOAtestResults.parseReportXML(report);
+        results.writeFailsafeSummary(actualSummary);
+
+        String actual = new String(Files.readAllBytes(actualSummary));
+        String expected = new String(Files.readAllBytes(expectedSummary.toPath()));
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void writeFailsafeSummary_WhenReportHasTenTestsAndTwoFail_WritesCompletedTenFailuresTwo() throws Exception {
+        File report = new File("target/test-classes/report_with_total_ten_fail_two.xml");
+        assertNotNull(report);
+        assertTrue(report.exists());
+
+        File expectedSummary = new File("target/test-classes/failsafe_summary_completed_ten_failures_two.xml");
+        assertNotNull(expectedSummary);
+        assertTrue(expectedSummary.exists());
+
+        Path actualSummary = Paths.get("target/test-classes/failsafe-summary.xml");
+        assertNotNull(actualSummary);
+
+        SOAtestResults results = SOAtestResults.parseReportXML(report);
+        results.writeFailsafeSummary(actualSummary);
+
+        String actual = new String(Files.readAllBytes(actualSummary));
+        String expected = new String(Files.readAllBytes(expectedSummary.toPath()));
+        assertEquals(actual, expected);
+    }
+}
